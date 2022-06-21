041f68d1d5073f8ba7b51e1dc9f9c7aba3673192
Julia Sosnowski <92020375+Sayunix@users.noreply.github.com>
2022-06-21 13:38:40 +0200

    Fixes

--- api/youtube_api/youtube-api.js
+++ api/youtube_api/youtube-api.js
@@ -1,6 +1,6 @@
 
 
-fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=relaxation_music&regionCode=AT&type=video&videoEmbeddable=true&key=AIzaSyC07Y-J5kpjl3RlLt9vzOfGJ4SdGAixiyQ')
+fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=relevance&q=relaxation_music&regionCode=AT&type=video&videoEmbeddable=true&key=AIzaSyC07Y-J5kpjl3RlLt9vzOfGJ4SdGAixiyQ')
     .then((data)=>{
         return data.json();
     }).then((result)=>{
@@ -9,7 +9,7 @@ fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=
     let videoContainer = document.querySelector(".youtube-container"); //video container created
     for(let video of videos){ // for each
         videoContainer.innerHTML += `
-        <iframe width="560" height="400" src="https://www.youtube.com/embed/${video.id.videoId}" title="YouTube video player" 
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${video.id.videoId}" title="YouTube video player" 
         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <p></p>`
     }
