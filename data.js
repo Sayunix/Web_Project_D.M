4f2ef48c5aecac82741cede69ee3123eb81f91f0
Strearm <92020515+Strearm@users.noreply.github.com>
2022-06-22 00:09:32 +0200

    Update data.js

--- data.js
+++ data.js
@@ -5,10 +5,10 @@ const ROLE = {
 module.exports ={
     ROLE: ROLE,
     users: [
-        {id: 1, name: 'Armin', password: 'mein', role: ROLE.ADMIN},
-        {id: 2, name: 'Safa',password: 'beidl', role: ROLE.ADMIN},
-        {id: 3, name: 'Julia',password: 'brennt', role: ROLE.ADMIN},
-        {id: 4, name: 'Nico',password:'hilfe', role: ROLE.ADMIN},
+        {id: 1, name: 'Armin', password: 'armin', role: ROLE.ADMIN},
+        {id: 2, name: 'Safa',password: 'safa', role: ROLE.ADMIN},
+        {id: 3, name: 'Julia',password: 'julia', role: ROLE.ADMIN},
+        {id: 4, name: 'Nico',password:'nico', role: ROLE.ADMIN},
         {id: 5, name: 'Rando',password: '!', role: ROLE.BASIC}
     ]
 }
